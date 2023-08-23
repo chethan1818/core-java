a6eed9f06ed385bda4600949fcd6880c0d0a3f3f
chethan80 <hkchethan57@gmail.com>
2023-08-23 14:08:07 +0530

    messga

---
+++ SportShop.java
@@ -0,0 +1,14 @@
+class SportShop{
+
+static String sportMaterial[] = {"Cricket Bat", "Football", "volleyball","Hockey", "leather Ball", "carrom board", "Trophies"};
+
+static int price[] ={1000,2000,1200,5000,345,550,2000};
+public static void main (String arg[]) {
+System.out.println( "The SportMaterial is" + sportMaterial.length);
+for(int chethanIndex=0 ; chethanIndex < sportMaterial.length;chethanIndex++) {
+System.out.println(sportMaterial[chethanIndex]);
+
+}
+
+}
+}
